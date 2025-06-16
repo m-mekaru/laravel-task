9275abba6cb8801da6437fa97c728e0aac7aafe5
m-mekaru <m.mekaru41@gmail.com>
2025-06-17 00:47:35 +0900

    create route & controller

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PostsController;
+
+Route::get('/index', [PostsController::class, 'index']);
+
+Route::get('/show', [PostsController::class, 'show']);
